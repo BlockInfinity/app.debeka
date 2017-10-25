3d1d7f279913418d67f8388aba82b5f009acd5b2
Magnus Gödde <Magnus.Goedde@gmx.de>
2017-10-25 16:11:14 +0200

    added metamask

--- app/index.js
+++ app/index.js
@@ -11,8 +11,15 @@ import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './../node_modules/font-awesome/css/font-awesome.css'
 import './../node_modules/animate.css/animate.min.css'
 import './../public/styles/style.css'
+import './scripts/metamask.js'
+
+addEventListener('accountLoaded', function(e) {
+    console.log("event accountLoaded received!!")
+    console.log(web3.eth.accounts) 
+}, false);
+
 
 ReactDOM.render(
-<Router history={hashHistory}>{routes}</Router>,
+<Router history={hashHistory} >{routes}</Router>,
     document.getElementById('root')
 );
--- app/views/Main.js
+++ app/views/Main.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+
+
 class Main extends Component {
 
     render() {
@@ -9,11 +12,8 @@ class Main extends Component {
                     <div className="col-lg-12">
                         <div className="text-center m-t-lg">
                             <h1>
-                                Welcome in INSPINIA ReactJS Seed Project
+                                Current Metamask Account: {web3.eth.accounts}
                             </h1>
-                            <small>
-                                It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
-                            </small>
                         </div>
                     </div>
                 </div>
