128ea906753806791da7f48c771fd80b1a1d8d8a
Matthias Stumpp <mstumpp@gmail.com>
2017-10-25 18:59:05 +0200

    Api.js hinzugefügt

---
+++ app/scripts/api.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const Web3 = require("web3");
+
+// gibt true zurück, wenn Account in MetaMask geladen ist
+function isAccountLoaded(_address) {
+  return true;
+}
+
+// gibt Account Stand zurück
+function accountStatus(_address) {
+  return 123;
+}
+
+// initiales anlegen eines energy system
+function createEnergySystem(_anzahlTokens, _name, _beschreibung) {
+
+}
+
+function getEnergySystemData(_address) {
+
+}
+
+// verkaufen von shares
+function sellEnergySystemShares(_address, _anzahlTokens, _price) {
+
+}
+
+// list of anzahlToken/price pairs
+function getSellEnergySystemShares(_address) {
+
+}
+
+function buyEnergySystemShares(_address, _anzahlTokens, _price) {
+
+}
+
+// list of anzahlToken/price pairs
+function getBuyEnergySystemShares(_address) {
+
+}
+
+module.exports = {
+
+}
---
+++ app/scripts/server.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Web3 = require("web3");
+
+function registerAskOrders() {
+
+}
+
+function registerBidOrders() {
+
+}
+
+function registerMatchedOrders() {
+
+}
+
+module.exports = {
+ 
+}
