1583009097c55238ead840b92ae75767cee0544a
Magnus Gödde <Magnus.Goedde@gmx.de>
2017-10-27 20:05:38 +0200

    introduced module.export in api.js

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // gibt true zurück, wenn Account in MetaMask geladen ist
-window.isAccountLoaded = function(_address) {
+module.exports.isAccountLoaded = function() {
     if (typeof web3 !== 'undefined') {
         window.web3 = new Web3(web3.currentProvider);
         if (web3.eth.accounts.length > 0) {
@@ -15,32 +15,38 @@ window.isAccountLoaded = function(_address) {
 
 
 // gets default account that is used by metamask 
-window.getDefaultAccount = function() {
+module.exports.getDefaultAccount = function() {
     return web3.eth.defaultAccount;
 }
 
 
 // gibt Account Stand zurück
-window.getEtherBalance = function(_user = web3.eth.defaultAccount) {
-    return web3.eth.getBalance(_user);
+module.exports.getEtherBalance = function(_user = web3.eth.defaultAccount) {
+    return new Promise((resolve, reject) => {
+        web3.eth.getBalance(_user, function(err, res) {
+            if (err)
+                reject(err);
+            else
+                resolve(res.toNumber());
+        });
+    })
 }
 
 
 // initiales anlegen eines energysystemtoken contracts. Jener contract wird über eine contract factory erstellt. Auf server seite wird sicher gestellt, dass jene factory deployed ist. 
-window.createEnergySystemToken = function(_anzahlTokens, _name, _beschreibung) {
+module.exports.createEnergySystemToken = function(_anzahlTokens, _name, _beschreibung) {
 
-    return fetch('./EnergySystemTokenFactory').then(response => {
+    return fetch('/EnergySystemTokenFactory').then(response => {
         console.log("got reponse");
         if (response.status !== 200) {
             let msg = `Looks like there was a problem. Status Code: + ${response.status}`
-            reject(msg);
+            return Promise.reject(msg);
         }
         return response.json().then(function(data) {
             let abi = data.abi;
             let address = data.address;
             let contract = web3.eth.contract(abi);
-            window.contract = contract;
-            window.factory = contract.at(address);
+            let factory = contract.at(address);
             return new Promise((resolve, reject) => {
                 factory.createEnergySystemToken((error, txhash) => {
                     if (error) {
@@ -60,20 +66,9 @@ window.createEnergySystemToken = function(_anzahlTokens, _name, _beschreibung) {
     })
 }
 
-
-// // retrieves all EnergySystemTokenCreationEvents for logged in user.  
-// window.getEnergySystemTokenCreationEvents = function(_user = web3.eth.defaultAccount) {
-//     return fetch(`./EnergySystemTokens?userAddress=${web3.eth.defaultAccount}`).then(response => {
-//         return response.json();
-//     }).then(res => {
-//         return res;
-//     });
-// }
-
-
 // gibt die adresse des zuletzt erstellten energysystemtoken contracts wieder 
-window.getLastEnergySystemTokenAddressForUser = function(_user = web3.eth.defaultAccount) {
-    return fetch(`./LastEnergySystemTokenAddressForUser?userAddress=${web3.eth.defaultAccount}`).then(response => {
+module.exports.getLastEnergySystemTokenAddressForUser = function(_user = web3.eth.defaultAccount) {
+    return fetch(`/LastEnergySystemTokenAddressForUser?userAddress=${web3.eth.defaultAccount}`).then(response => {
         return response.json();
     }).then(res => {
         return res;
@@ -82,8 +77,8 @@ window.getLastEnergySystemTokenAddressForUser = function(_user = web3.eth.defaul
 
 
 // gibt alle energy system token addresses für einen nutzer zurück 
-window.getAllEnergySystemTokenAddressesForUser = function(_user = web3.eth.defaultAccount) {
-    return fetch(`./AllEnergySystemTokenAddressesForUser?userAddress=${web3.eth.defaultAccount}`).then(response => {
+module.exports.getAllEnergySystemTokenAddressesForUser = function(_user = web3.eth.defaultAccount) {
+    return fetch(`/AllEnergySystemTokenAddressesForUser?userAddress=${web3.eth.defaultAccount}`).then(response => {
         return response.json();
     }).then(res => {
         return res;
@@ -92,8 +87,8 @@ window.getAllEnergySystemTokenAddressesForUser = function(_user = web3.eth.defau
 
 
 // gibt alle energy system token adressen zurück
-window.getAllEnergySystemTokenAddresses = function() {
-    return fetch(`./AllEnergySystemTokenAddresses`).then(response => {
+module.exports.getAllEnergySystemTokenAddresses = function() {
+    return fetch(`/AllEnergySystemTokenAddresses`).then(response => {
         return response.json();
     }).then(res => {
         return res;
@@ -101,68 +96,71 @@ window.getAllEnergySystemTokenAddresses = function() {
 }
 
 
-window.getTokenBalance = function(_energySystemTokenAddress, _user = web3.eth.defaultAccount) {
+module.exports.getTokenBalance = function(_energySystemTokenAddress, _user = web3.eth.defaultAccount) {
 
 }
 
 
-window.getTotalNumberOfTokens = function(_energySystemTokenAddress) {
+module.exports.getTotalNumberOfTokens = function(_energySystemTokenAddress) {
 
 }
 
 // returns {price, orders[]} for a specific _energySystemTokenAddress
-window.getFulfilledOrders = function(_energySystemTokenAddress) {
+module.exports.getFulfilledOrders = function(_energySystemTokenAddress) {
 
 }
 
 
-window.getRaisedEther = function(_energySystemTokenAddress) {
+module.exports.getRaisedEther = function(_energySystemTokenAddress) {
 
 }
 
 
 // Kauf von tokens während der funding phase 
-window.buyEnergySystemTokens = function(_energySystemTokenAddress, _value) {
+module.exports.buyEnergySystemTokens = function(_energySystemTokenAddress, _value) {
     // - sol function buyEnergySystemTokens()  
 }
 
 
 // Zurücktauschen von tokens gegen ether während der funding phase 
-window.getMoneyBack = function(_energySystemTokenAddress) {
+module.exports.getMoneyBack = function(_energySystemTokenAddress) {
     // - sol function getMoneyBack() 
 }
 
 
-window.getTimeLeftForFundingPhase = function(_energySystemTokenAddress) {
+module.exports.getTimeLeftForFundingPhase = function(_energySystemTokenAddress) {
 
 }
 
 
 // erträge im energysystemtoken contract ausbezahlen lassen 
-window.withdraw = function(_energySystemTokenAddress) {
+module.exports.withdraw = function(_energySystemTokenAddress) {
 
 }
 
 
 //  function for future trading enhancement
-window.transferEther = function(_to, _value) {
+module.exports.transferEther = function(_to, _value) {
 
 }
 
 
 //  function for future trading enhancement
-window.transferEnergySystemShares = function(_to, _value) {
+module.exports.transferEnergySystemShares = function(_to, _value) {
 
 }
 
 
 //  function for future trading enhancement
-window.transferFromEnergySystemShares = function(_from, _to, _value) {
+module.exports.transferFromEnergySystemShares = function(_from, _to, _value) {
 
 }
 
 
 // ruft die enbw oder der besitzer auf um erträge an die shareholder auszuzahlen  
-window.disburse = function(_energySystemTokenAddress) {
+module.exports.disburse = function(_energySystemTokenAddress) {
 
 }
+
+// merges properties of both objects, so that i can access the functions in browser via window. 
+Object.assign(window, module.exports)
