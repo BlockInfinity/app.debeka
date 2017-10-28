4b06c413d40d2198e45cf54b24e9911fe757f9a1
Magnus Gödde <Magnus.Goedde@gmx.de>
2017-10-28 17:36:59 +0200

    refactoring

--- test.js
+++
@@ -1,5 +0,0 @@
-/***/ (function(module, exports) {
-
-	eval("// require('../scripts/api.js');\n// var expect = chai.expect;\n\ndescribe('Array', function() {\n    it('should return -1 when the value is not present', function() {\n      // expect(1).to.equal(1);\n    });\n});\n\n\n//////////////////\n// WEBPACK FOOTER\n// ./app/mochaTests/apiTest.test.js\n// module id = 0\n// module chunks = 0\n//# sourceURL=webpack:///./app/mochaTests/apiTest.test.js?");
-
-/***/ })
